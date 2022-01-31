22776e65527b8036369aafa508ceecca2333d636
Tyukhe <Vadim..ru1112@gmail.com>
2022-01-31 17:41:44 +0300

    Промежуток, он не меняет зум

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ pygame.init()
 pygame.display.set_caption('MapAPI')
 screen = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
 ButtonOut = Button(screen, 'Выход', (screen.get_width() - 180, screen.get_height() - 80), (160, 60), BLUE, BlACK)
-ImagSur = Image(screen, image("37.530887", "55.703118", "0.002"), (0, 0), (screen.get_width(), screen.get_height()))
+lon = "37.530887"
+lat = "55.703118"
+delta = "0.002"
+ImagSur = Image(screen, image(lon, lat, delta), (0, 0), (screen.get_width(), screen.get_height()))
 
 if __name__ == '__main__':
     running = True
@@ -22,6 +25,12 @@ if __name__ == '__main__':
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if ButtonOut.get_button().collidepoint(event.pos):
                     running = False
+            if pygame.event == pygame.K_PAGEUP:
+                delta = f'{int(delta) - 0.001}'
+                ImagSur.set_image(image(lon, lat, delta))
+            if pygame.event == pygame.K_PAGEDOWN:
+                delta = f'{int(delta) + 0.001}'
+                ImagSur.set_image(image(lon, lat, delta))
 
         ImagSur.draw_image()
         ButtonOut.draw_button()
